d805f068ec4e8256a0b364dd221af9d232707e9a
Marcel Hellkamp <marc@gsites.de>
2009-07-30 11:15:17 +0200

    Workaround for Python 3.0 SyntaxErrors when importing bottle pre-2to3

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 import sys
-import bottle
+import os.path
+from distutils.core import setup
+
+if sys.version_info < (2,5):
+    raise NotImplementedError("Sorry, you need at least Python 2.5 or Python 3.x to use bottle.")
 
 try:
     from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
     from distutils.command.build_py import build_py
 
-if sys.version_info < (2,5):
-    raise NotImplementedError("Sorry, you need at least Python 2.6 or Python 3.x to use bottle.")
+# Ugly, but we can't import bottle with 3.x to read __version__ (throws SyntaxError)
+for line in open(os.path.join(os.path.dirname(sys.argv[0]),'bottle.py')):
+    if line.startswith('__version__'):
+        version = eval(line.split('=')[-1])
 
 setup(name='bottle',
-      version=bottle.__version__,
+      version=version,
       description='Fast and simple WSGI-framework for small web-applications.',
       long_description='Bottle is a fast and simple micro-framework for small web-applications. It offers request dispatching (Routes) with url parameter support, Templates, key/value Databases, a build-in HTTP Server and adapters for many third party WSGI/HTTP-server and template engines. All in a single file and with no dependencies other than the Python Standard Library.',
       author='Marcel Hellkamp',
@@ -22,7 +27,6 @@ setup(name='bottle',
       py_modules=['bottle'],
       license='MIT',
       platforms = 'any',
-      zip_safe = True,
       classifiers=['Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
