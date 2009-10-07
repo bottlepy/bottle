9ef63f3719ae5877a57f878271975db9454d2d3f
Marcel Hellkamp <marc@gsites.de>
2009-10-07 02:33:36 +0200

    Formating and minor Python3 bug in test file.

--- test/test_sendfile.py
+++ test/test_sendfile.py
@@ -32,22 +32,26 @@ class TestDateParser(unittest.TestCase):
 class TestSendFile(unittest.TestCase):
     def test_valid(self):
         """ SendFile: Valid requests"""
-        try: send_file(os.path.basename(__file__), root='./')
+        try:
+            send_file(os.path.basename(__file__), root='./')
         except BreakTheBottle, e:
-            self.assertEqual(open(__file__).read(), e.output.read())
+            self.assertEqual(open(__file__,'rb').read(), e.output.read())
 
     def test_invalid(self):
         """ SendFile: Invalid requests"""
-        try: send_file('not/a/file', root='./')
+        try:
+            send_file('not/a/file', root='./')
         except HTTPError, e:
             self.assertEqual(404, e.http_status)
-        try: send_file(os.path.join('./../', os.path.basename(__file__)), root='./views/')
+        try:
+            send_file(os.path.join('./../', os.path.basename(__file__)), root='./views/')
         except HTTPError, e:
             self.assertEqual(401, e.http_status)
         try:
             fp, fn = tempfile.mkstemp()
             os.chmod(fn, 0)
-            try: send_file(fn, root='/')
+            try:
+                send_file(fn, root='/')
             except HTTPError, e:
                 self.assertEqual(401, e.http_status)
         finally:
@@ -56,27 +60,33 @@ class TestSendFile(unittest.TestCase):
             
     def test_mime(self):
         """ SendFile: Mime Guessing"""
-        try: send_file(os.path.basename(__file__), root='./')
+        try:
+            send_file(os.path.basename(__file__), root='./')
         except BreakTheBottle, e:
             self.assertTrue(response.content_type in ('application/x-python-code', 'text/x-python'))
-        try: send_file(os.path.basename(__file__), root='./', mimetype='some/type')
+        try:
+            send_file(os.path.basename(__file__), root='./', mimetype='some/type')
         except BreakTheBottle, e:
             self.assertEqual('some/type', response.content_type)
-        try: send_file(os.path.basename(__file__), root='./', guessmime=False)
+        try:
+            send_file(os.path.basename(__file__), root='./', guessmime=False)
         except BreakTheBottle, e:
             self.assertEqual('text/plain', response.content_type)
 
     def test_ims(self):
         """ SendFile: If-Modified-Since"""
         request.environ['HTTP_IF_MODIFIED_SINCE'] = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
-        try: send_file(os.path.basename(__file__), root='./')
+        response.bind()
+        try:
+            send_file(os.path.basename(__file__), root='./')
         except HTTPError, e:
             self.assertEqual(304 ,e.http_status)
 
         request.environ['HTTP_IF_MODIFIED_SINCE'] = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(100))
-        try: send_file(os.path.basename(__file__), root='./')
+        try:
+            send_file(os.path.basename(__file__), root='./')
         except BreakTheBottle, e:
-            self.assertEqual(open(__file__).read(), e.output.read())
+            self.assertEqual(open(__file__,'rb').read(), e.output.read())
 
 
 
