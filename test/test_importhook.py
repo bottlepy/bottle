823f5b16fde937b5e1f82e4791ba82818cab39d4
Marcel Hellkamp <marc@gsites.de>
2011-04-28 01:10:14 +0200

    Added tests for bottle.ext import hooks.

---
+++ test/test_importhook.py
@@ -0,0 +1,38 @@
+﻿# -*- coding: utf-8 -*-
+import unittest
+import sys
+import imp
+
+
+class TestImportHooks(unittest.TestCase):
+    server = 'wsgiref'
+
+    def make_module(self, name, **args):
+        mod = sys.modules.setdefault(name, imp.new_module(name))
+        mod.__file__ = '<virtual %s>' % name
+        mod.__dict__.update(**args)
+        return mod
+    
+    def test_direkt_import(self):
+        mod = self.make_module('bottle_test')
+        import bottle.ext.test
+        self.assertEqual(bottle.ext.test, mod)
+
+    def test_from_import(self):
+        mod = self.make_module('bottle_test')
+        from bottle.ext import test
+        self.assertEqual(test, mod)
+
+    def test_data_import(self):
+        mod = self.make_module('bottle_test', item='value')
+        from bottle.ext.test import item
+        self.assertEqual(item, 'value')
+    
+    def test_import_fail(self):
+        ''' Test a simple static page with this server adapter. '''
+        def test():
+            import bottle.ext.doesnotexist
+        self.assertRaises(ImportError, test)
+
+if __name__ == '__main__': #pragma: no cover
+    unittest.main()
