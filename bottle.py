93732849ffafb44c52d967e81cc9754b81266e40
Marcel Hellkamp <marc@gsites.de>
2011-05-30 16:06:43 +0200

    MultiDict and HeaderDict cleanup and speedup.
    api: MultiDict.[iter]items() now returns all items. iterallitems() is obsolete.

--- bottle.py
+++ bottle.py
@@ -1235,53 +1235,59 @@ class _ImportRedirect(object):
 
 
 class MultiDict(DictMixin):
-    """ A dict that remembers old values for each key """
-    # collections.MutableMapping would be better for Python >= 2.6
-    def __init__(self, *a, **k):
-        self.dict = dict()
-        for k, v in dict(*a, **k).iteritems():
-            self[k] = v
+    """ A dict that stores multiple values per key. """
 
+    def __init__(self, *a, **k):
+        self.dict = dict((k, [v]) for k, v in dict(*a, **k).iteritems())
     def __len__(self): return len(self.dict)
     def __iter__(self): return iter(self.dict)
     def __contains__(self, key): return key in self.dict
     def __delitem__(self, key): del self.dict[key]
-    def keys(self): return self.dict.keys()
-    def __getitem__(self, key): return self.get(key, KeyError, -1)
+    def __getitem__(self, key): return self.dict[key][-1]
     def __setitem__(self, key, value): self.append(key, value)
+    def keys(self): return self.dict.keys()
+    def items(self): return list(self.iteritems())
+
+    def iteritems(self):
+        ''' Iterate over all (key, value) tuples for each value of each key.'''
+        for key, values in self.dict.iteritems():
+            for value in values:
+                yield key, value
+
+    def iterallitems(self):
+        depr('Iterating over all items is now the default for MultiDict.iteritems()')
+        return self.iteritems()
 
     def append(self, key, value): self.dict.setdefault(key, []).append(value)
     def replace(self, key, value): self.dict[key] = [value]
     def getall(self, key): return self.dict.get(key) or []
 
     def get(self, key, default=None, index=-1):
-        if key not in self.dict and default != KeyError:
-            return [default][index]
-        return self.dict[key][index]
+        if key in self.dict or default is KeyError:
+            return self.dict[key][index]
+        return default
 
-    def iterallitems(self):
-        for key, values in self.dict.iteritems():
-            for value in values:
-                yield key, value
 
+def _hkey(s):
+    return s.title().replace('_','-')
 
 class HeaderDict(MultiDict):
-    """ Same as :class:`MultiDict`, but title()s the keys and overwrites. """
-    def __contains__(self, key):
-        return MultiDict.__contains__(self, self.httpkey(key))
-    def __getitem__(self, key):
-        return MultiDict.__getitem__(self, self.httpkey(key))
-    def __delitem__(self, key):
-        return MultiDict.__delitem__(self, self.httpkey(key))
-    def __setitem__(self, key, value): self.replace(key, value)
-    def get(self, key, default=None, index=-1):
-        return MultiDict.get(self, self.httpkey(key), default, index)
+    """ A case-insensitive version of :class:`MultiDict` that defaults to
+        replace the old value instead of appending it. """
+
+    def __init__(self, *a, **k):
+        self.dict = dict((_hkey(k), [str(v)]) for k, v in dict(*a, **k).iteritems())
+    def __contains__(self, key): return _hkey(key) in self.dict
+    def __delitem__(self, key): del self.dict[_hkey(key)]
+    def __getitem__(self, key): return self.dict[_hkey(key)][-1]
+    def __setitem__(self, key, value): self.dict[_hkey(key)] = [str(value)]
     def append(self, key, value):
-        return MultiDict.append(self, self.httpkey(key), str(value))
-    def replace(self, key, value):
-        return MultiDict.replace(self, self.httpkey(key), str(value))
-    def getall(self, key): return MultiDict.getall(self, self.httpkey(key))
-    def httpkey(self, key): return str(key).replace('_','-').title()
+        self.dict.setdefault(_hkey(key), []).append(str(value))
+    def replace(self, key, value): self.dict[_hkey(key)] = [str(value)]
+    def getall(self, key): return self.dict.get(_hkey(key)) or []
+    def get(self, key, default=None, index=-1):
+        return super(HeaderDict, self).get(_hkey(key), default, index)
+
 
 
 class WSGIHeaderDict(DictMixin):
