7831617b21732037adb50d55b5997b3aa04b9154
Marcel Hellkamp <marc@gsites.de>
2011-06-13 15:53:49 +0200

    Fix #169 "Bottle-SQLite doesn't import HTTPError" Thanks fcamel :)

--- plugins/sqlite/bottle_sqlite.py
+++ plugins/sqlite/bottle_sqlite.py
@@ -27,13 +27,14 @@ Usage Example::
 '''
 
 __author__ = "Marcel Hellkamp"
-__version__ = '0.1'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 
 ### CUT HERE (see setup.py)
 
 import sqlite3
 import inspect
+from bottle import HTTPError
 
 
 class SQLitePlugin(object):
