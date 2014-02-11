8cc88f34302b1b06cf573a59b249beaca86ae90b
apapas <adonis.papaderos@gmail.com>
2014-02-11 17:17:32 +0200

    updated route test to use functools.wraps

--- test/test_route.py
+++ test/test_route.py
@@ -1,5 +1,6 @@
 import unittest
 import bottle
+import functools
 from tools import api
 
 
@@ -9,6 +10,7 @@ class TestRoute(unittest.TestCase):
     def test_callback_inspection(self):
         def x(a, b): pass
         def d(f):
+            @functools.wraps(f)
             def w():
                 return f()
             return w
@@ -19,6 +21,7 @@ class TestRoute(unittest.TestCase):
 
         def d2(foo):
             def d(f):
+                @functools.wraps(f)
                 def w():
                     return f()
                 return w
@@ -31,6 +34,6 @@ class TestRoute(unittest.TestCase):
         def d3(a, b):
             return ''
 
-        route = bottle.Route(None, None, None, bottle.view(d3))
+        route = bottle.Route(None, None, None, bottle.view('index')(d3))
         self.assertEqual(route.get_undecorated_callback(), d3)
         self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
