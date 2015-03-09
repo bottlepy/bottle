3bb2d5fecbe27c0ff47ff672c22b3693f462d394
Alex Collins <alextcollins@gmail.com>
2015-03-09 19:11:21 +0000

    Issue #727 demonstrate "oo" routing and test to break wsgi.input lookup in environ

---
+++ test/test_oorouting.py
@@ -0,0 +1,34 @@
+"""
+Tests & demonstrates various OO approaches to routes
+"""
+import unittest
+from io import BytesIO
+
+__author__ = 'atc'
+
+from bottle import Bottle, request, tob, BaseRequest
+
+
+class TestRouter(object):
+    """
+    A test class for wrapping routes to test certain OO scenarios
+    """
+
+    app = Bottle()
+
+    @app.post("/route1/<msg>")
+    def route_1(self, msg):
+        body = request.body.readline()
+        return {'msg': msg, 'len': len(body)}
+
+
+class TestRoutes(unittest.TestCase):
+    def test_route1(self):
+        body = "abc"
+        request.environ['CONTENT_LENGTH'] = str(len(tob(body)))
+        request.environ['wsgi.input'] = BytesIO()
+        request.environ['wsgi.input'].write(tob(body))
+        request.environ['wsgi.input'].seek(0)
+
+        result = TestRouter().route_1("bob")
+        self.assertEqual(result, dict(msg="bob", len=3))
