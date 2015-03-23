54b106f7d17acb675e6bff626eb7c7a7e4ba94cb
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-23 10:06:20 +0100

    Added tests of the .get_undecorated_callback() bugfix.
    See commit no. 66a9264ad880c3e3d009b3ac79478963e232218f for bugfix details.

--- test/test_route.py
+++ test/test_route.py
@@ -28,3 +28,39 @@ class TestRoute(unittest.TestCase):
         self.assertEqual(route.get_undecorated_callback(), x)
         self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
 
+    def test_callback_inspection_multiple_args(self):
+        def x(a, b):
+            pass
+
+        # decorator with multiple arguments
+        def d2(fn=None, arg1="something", arg2="something else"):
+            def d(f):
+                def w(*args, **kwargs):
+                    return f()
+                return w
+
+            # support for calling this decorator without arguments
+            if fn:
+                return d(fn)
+
+            return d
+
+        # both decorator parameters
+        route = bottle.Route(None, None, None, d2(arg1='foo', arg2='bar')(x))
+        self.assertEqual(route.get_undecorated_callback(), x)
+        self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
+
+        # first decorator parameter
+        route = bottle.Route(None, None, None, d2(arg1='foo')(x))
+        self.assertEqual(route.get_undecorated_callback(), x)
+        self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
+
+        # second decorator parameter
+        route = bottle.Route(None, None, None, d2(arg2='bar')(x))
+        self.assertEqual(route.get_undecorated_callback(), x)
+        self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
+
+        # without decorator parameters
+        route = bottle.Route(None, None, None, d2()(x))
+        self.assertEqual(route.get_undecorated_callback(), x)
+        self.assertEqual(set(route.get_callback_args()), set(['a', 'b']))
