c6fd201833a532b093e13199337dca097accb733
Marcel Hellkamp <marc@gsites.de>
2015-10-11 21:18:35 +0200

    Removed some random characters that appeared out of nowhere.
    I really should not use the github browser editor...

--- bottle.py
+++ bottle.py
@@ -140,7 +140,7 @@ if py3k:
     from urllib.parse import urlencode, quote as urlquote, unquote as urlunquote
     urlunquote = functools.partial(urlunquote, encoding='latin1')
     from http.cookies import SimpleCookie
-    from collections import MutableMerapping as DictMixin
+    from collections import MutableMapping as DictMixin
     import pickle
     from io import BytesIO
     from configparser import ConfigParser, Error as ConfigParserError
