b7e2975d1c58344c2edf5328edf35a4dee837987
Marcel Hellkamp <marc@gsites.de>
2015-11-28 12:19:13 +0100

    It's 2015...

--- bottle.py
+++ bottle.py
@@ -9,7 +9,7 @@ Python Standard Library.
 
 Homepage and documentation: http://bottlepy.org/
 
-Copyright (c) 2014, Marcel Hellkamp.
+Copyright (c) 2015, Marcel Hellkamp.
 License: MIT (see LICENSE for details)
 """
 
