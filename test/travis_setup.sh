24c970df31aff0b0cd3ce9a7cc2711b073b40e20
Marcel Hellkamp <marc@gsites.de>
2018-02-14 11:41:56 +0100

    travis: Use deadsnakes/ppa instead of fkrull/deadsnakes to install old python releases.

--- test/travis_setup.sh
+++ test/travis_setup.sh
@@ -4,12 +4,11 @@ set -e
 set -x
 
 if test -n "$PY"; then
-  # https://github.com/pycurl/pycurl/blob/master/tests/travis/setup.sh
-  sudo add-apt-repository -y ppa:fkrull/deadsnakes
+  sudo add-apt-repository -y ppa:deadsnakes/ppa
   sudo apt-get update
   sudo apt-get install -y --allow-unauthenticated python$PY-dev
 
-  if test "$PY" = 2.5; then
+  if test "$PY" = "2.5"; then
     wget https://pypi.python.org/packages/source/s/simplejson/simplejson-3.6.3.tar.gz
     tar -xvzf simplejson-3.6.3.tar.gz
     cd simplejson-3.6.3
