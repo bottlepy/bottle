84c1ee14e1717ec63782dd5a159fe5848fce1cc4
cclauss <cclauss@bluewin.ch>
2018-07-19 13:39:12 +0200

    Add Python 3.6 and 3.7 to PyPI page
    Should modify the bottom left of https://pypi.org/project/bottle on the next [release](https://github.com/bottlepy/bottle/releases).

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup(name='bottle',
                    'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
                    ],
       )
