b0ce05c2a9e6a3e1ff01c5cbd18f7806df5bf7d6
Marcel Hellkamp <marc@gsites.de>
2018-12-01 18:26:43 +0100

    Added Python 3.4-3.7 language classifiers

--- setup.py
+++ setup.py
@@ -36,8 +36,9 @@ setup(name='bottle',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         ],
      )
-
-
-
