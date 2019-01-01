6f5e1af31055d5c76ca0d4e009a88c3e81df5086
Sebastian Pipping <sebastian@pipping.org>
2019-01-01 21:34:55 +0100

    travis: Resolve some travis_setup.sh code duplication

--- test/travis_setup.sh
+++ test/travis_setup.sh
@@ -13,15 +13,19 @@ pip install -U coverage
 pip install coveralls
 
 # Server back-ends and template engines. Not all back-ends support all python versions and we only want to test for 2.7 and 3.6 to keep things sane
-if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then
+case ${TRAVIS_PYTHON_VERSION} in
+2.7|3.6)
     sudo apt-get update -y
     sudo apt-get install -y libev-dev
     pip install mako jinja2 waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld gunicorn eventlet
-    pip install flup fapws3 rocket bjoern gevent
-elif  [[ $TRAVIS_PYTHON_VERSION == 3.6 ]]; then
-    sudo apt-get update -y
-    sudo apt-get install -y libev-dev
-    pip install mako jinja2 waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld gunicorn eventlet
-    pip install uvloop
-fi
+    ;;
+esac
 
+case ${TRAVIS_PYTHON_VERSION} in
+2.7)
+    pip install flup fapws3 rocket bjoern gevent
+    ;;
+3.6)
+    pip install uvloop
+    ;;
+esac
