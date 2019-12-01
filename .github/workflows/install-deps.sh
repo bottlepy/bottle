c9dd6808bb8363014c39865cc8f03fc4f2d8cfb0
Marcel Hellkamp <marc@gsites.de>
2019-12-01 19:14:09 +0100

    Move to github actions (#1183)

---
+++ .github/workflows/install-deps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# Test utilities
+pip install -U pip pytest coverage
+
+# Test dependencies (Server back-ends and template engines)
+sudo apt-get install -y libev-dev
+pip install mako jinja2
+
+for name in waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld\
+            gunicorn eventlet flup fapws3 rocket bjoern gevent uvloop; do
+    pip install $name || echo "Failed to install $name with $(python -V 2>&1)" 1>&2
+done
