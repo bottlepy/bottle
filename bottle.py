1a40a42b19f752a600976b76da7d74e6be56d2e6
张酉夫 <zhangyoufu@gmail.com>
2019-12-19 12:28:58 +0800

    refine parse_date

--- bottle.py
+++ bottle.py
@@ -2980,7 +2980,7 @@ def parse_date(ims):
     """ Parse rfc1123, rfc850 and asctime timestamps and return UTC epoch. """
     try:
         ts = email.utils.parsedate_tz(ims)
-        return time.mktime(ts[:8] + (0, )) - (ts[9] or 0) - time.timezone
+        return calendar.timegm(ts[:8] + (0, )) - (ts[9] or 0)
     except (TypeError, ValueError, IndexError, OverflowError):
         return None
 
