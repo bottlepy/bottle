405dee94cda746c76ba3ecc50f2364458a279e39
Martin Lehoux <martin@lehoux.net>
2020-01-28 23:07:16 +0100

    fix - Typo
    How dumb I was ... ^^
    Also grouped imports

--- bottle.py
+++ bottle.py
@@ -69,10 +69,10 @@ if __name__ == '__main__':
 ###############################################################################
 
 
-import base64, calendar, cgi, email.utils, functools, hmac, types, itertools,\
+import base64, calendar, cgi, email.utils, functools, hmac, itertools,\
        mimetypes, os, re, tempfile, threading, time, warnings, weakref, hashlib
 
-from types import FunctionType
+from types import FunctionType, ModuleType
 from datetime import date as datedate, datetime, timedelta
 from tempfile import TemporaryFile
 from traceback import format_exc, print_exc
@@ -2059,7 +2059,7 @@ class _ImportRedirect(object):
         """ Create a virtual package that redirects imports (see PEP 302). """
         self.name = name
         self.impmask = impmask
-        self.module = sys.modules.setdefault(name, types.Module(name))
+        self.module = sys.modules.setdefault(name, ModuleType(name))
         self.module.__dict__.update({
             '__file__': __file__,
             '__path__': [],
