d2a91df453c40bdd48aac329100e8aa5869866c2
Oz N Tiram <oz.tiram@gmail.com>
2020-09-16 13:02:29 +0200

    Add Python3.8 and Python3.9 classifieres
    This are tested and should work for all users running
    Python3.8 and Python3.9.

--- setup.py
+++ setup.py
@@ -35,5 +35,7 @@ setup(name='bottle',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
                    ],
       )
