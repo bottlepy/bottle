fbe608eb741cfd35ab9a0d101c6b64c52a3ed916
Oz N Tiram <oz.tiram@gmail.com>
2020-09-16 13:00:28 +0200

    Drop Python version 3.5
    This version is EOL since 2020-09-05.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 
 if sys.version_info < (2, 7):
-    raise NotImplementedError("Sorry, you need at least Python 2.7 or Python 3.5+ to use bottle.")
+    raise NotImplementedError("Sorry, you need at least Python 2.7 or Python 3.6+ to use bottle.")
 
 import bottle
 
@@ -33,7 +33,6 @@ setup(name='bottle',
                    'Topic :: Software Development :: Libraries :: Application Frameworks',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
                    ],
