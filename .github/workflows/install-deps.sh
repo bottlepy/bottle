d194abb413cae622e4dbd30c06ee2e8ea1f513d7
Christian Clauss <cclauss@me.com>
2020-11-14 23:17:57 +0100

    thread.isAlive() was removed in Python 3.9

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -9,7 +9,7 @@ pip install -U pip pytest coverage
 sudo apt-get install -y libev-dev
 pip install mako jinja2
 
-for name in waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld\
+for name in waitress cherrypy cheroot paste tornado twisted diesel meinheld\
             gunicorn eventlet flup fapws3 bjoern gevent aiohttp-wsgi uvloop; do
     pip install $name || echo "Failed to install $name with $(python -V 2>&1)" 1>&2
 done
