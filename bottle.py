ef7b678bda31b9d5c3f7f4cffcdcf14137b4cdad
Marcel Hellkamp <marc@gsites.de>
2022-06-12 20:54:46 +0200

    fix: view decorator does not forward default values if route result is None

--- bottle.py
+++ bottle.py
@@ -3682,7 +3682,7 @@ def view(tpl_name, **defaults):
                 tplvars.update(result)
                 return template(tpl_name, **tplvars)
             elif result is None:
-                return template(tpl_name, defaults)
+                return template(tpl_name, **defaults)
             return result
         return wrapper
     return decorator
