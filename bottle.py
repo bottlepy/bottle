c4b9e9fa0d3ed35c77f1e9a33f30d8033eaf819d
Marcel Hellkamp <marc@gsites.de>
2024-08-28 20:19:00 +0200

    docs: Update copyright year

--- bottle.py
+++ bottle.py
@@ -9,7 +9,7 @@ Python Standard Library.
 
 Homepage and documentation: http://bottlepy.org/
 
-Copyright (c) 2009-2018, Marcel Hellkamp.
+Copyright (c) 2009-2024, Marcel Hellkamp.
 License: MIT (see LICENSE for details)
 """
 
