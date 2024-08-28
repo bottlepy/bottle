1bf82df71d60e348344de79060a07cac2f0d44e3
Marcel Hellkamp <marc@gsites.de>
2024-08-28 15:55:44 +0200

    test: Remove server tests
    Those are integration tests, not unit tests and lead to flapping test results.

--- test/__init__.py
+++ test/__init__.py
@@ -9,11 +9,6 @@ try:
 except ImportError:
     pass
 
-
-if 'fast' in sys.argv:
-    sys.stderr.write("Warning: The 'fast' keyword skipps server tests.\n")
-    os.environ["TEST_FAST"] = "true"
-
 import bottle
 bottle.debug(True)
 
