39d192edadd964bd42134649cd7e85ea8970b369
Marcel Hellkamp <marc@gsites.de>
2024-08-28 15:52:28 +0200

    chore: Change actions to only test supported python versions

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -3,13 +3,5 @@ set -e
 set -x
 
 # Test utilities
-pip install -U pip pytest coverage
+pip install -U pytest pytest-cov mako jinja2
 
-# Test dependencies (Server back-ends and template engines)
-sudo apt-get install -y libev-dev
-pip install mako jinja2
-
-for name in waitress "cherrypy<9" cheroot paste tornado twisted diesel meinheld\
-            gunicorn eventlet flup bjoern gevent aiohttp-wsgi uvloop; do
-    pip install $name || echo "Failed to install $name with $(python -V 2>&1)" 1>&2
-done
