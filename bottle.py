eb2cd2b30ab20dceb55373c2c9c2ff04e435b529
Marcel Hellkamp <marc@gsites.de>
2024-09-05 13:38:31 +0200

    Release of 0.13.0

--- bottle.py
+++ bottle.py
@@ -17,7 +17,7 @@ from __future__ import print_function
 import sys
 
 __author__ = 'Marcel Hellkamp'
-__version__ = '0.13-dev'
+__version__ = '0.13.0'
 __license__ = 'MIT'
 
 ###############################################################################
