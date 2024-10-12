7528311c28a7a9e8807b69d22d40931e5a15b961
Marcel Hellkamp <marc@gsites.de>
2024-10-12 14:21:53 +0200

    fix: depr() should return DeprecationWarning

--- bottle.py
+++ bottle.py
@@ -185,6 +185,7 @@ def depr(major, minor, cause, fix, stacklevel=3):
     if DEBUG == 'strict':
         raise DeprecationWarning(text)
     warnings.warn(text, DeprecationWarning, stacklevel=stacklevel)
+    return DeprecationWarning(text)
 
 
 def makelist(data):  # This is just too handy
