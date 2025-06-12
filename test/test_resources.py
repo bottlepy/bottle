36740d109c226cdddff4ef794143d4f13881d595
Sakari Cajanus <scajanus@gmail.com>
2025-06-12 14:50:09 +0300

    Changes to make tests pass on Windows

--- test/test_resources.py
+++ test/test_resources.py
@@ -1,18 +1,27 @@
-from bottle import ResourceManager
 import os.path
+import sys
 import unittest
+from bottle import ResourceManager
+
+if sys.platform == 'win32':
+    TEST_PATHS = ('C:\\foo\\bar\\', 'C:\\foo\\bar\\baz', 'C:\\foo\\baz\\..\\bar\\blub')
+    EXPECTED = ['C:\\foo\\bar\\']
+else:
+    TEST_PATHS = ('/foo/bar/', '/foo/bar/baz', '/foo/baz/../bar/blub')
+    EXPECTED = ['/foo/bar/']
+
 
 class TestResourceManager(unittest.TestCase):
 
     def test_path_normalize(self):
-        tests = ('/foo/bar/', '/foo/bar/baz', '/foo/baz/../bar/blub')
-        for test in tests:
+        for test in TEST_PATHS:
             rm = ResourceManager()
             rm.add_path(test)
-            self.assertEqual(rm.path, ['/foo/bar/'])
+            self.assertEqual(rm.path, EXPECTED)
 
     def test_path_create(self):
-        import tempfile, shutil
+        import shutil
+        import tempfile
         tempdir = tempfile.mkdtemp()
         try:
             rm = ResourceManager()
@@ -24,8 +33,13 @@ class TestResourceManager(unittest.TestCase):
             shutil.rmtree(tempdir)
 
     def test_path_absolutize(self):
-        tests = ('./foo/bar/', './foo/bar/baz', './foo/baz/../bar/blub')
-        abspath = os.path.abspath('./foo/bar/') + os.sep
+        if sys.platform == 'win32':
+            tests = ('.\\foo\\bar\\', '.\\foo\\bar\\baz', '.\\foo\\baz\\..\\bar\\blub')
+            abspath = os.path.abspath('.\\foo\\bar\\') + os.sep
+        else:
+            tests = ('./foo/bar/', './foo/bar/baz', './foo/baz/../bar/blub')
+            abspath = os.path.abspath('./foo/bar/') + os.sep
+
         for test in tests:
             rm = ResourceManager()
             rm.add_path(test)
@@ -37,29 +51,36 @@ class TestResourceManager(unittest.TestCase):
             self.assertEqual(rm.path, [abspath])
 
     def test_path_unique(self):
-        tests = ('/foo/bar/', '/foo/bar/baz', '/foo/baz/../bar/blub')
         rm = ResourceManager()
-        [rm.add_path(test) for test in tests]
-        self.assertEqual(rm.path, ['/foo/bar/'])
+        [rm.add_path(test) for test in TEST_PATHS]
+        self.assertEqual(rm.path, EXPECTED)
 
     def test_root_path(self):
-        tests = ('/foo/bar/', '/foo/bar/baz', '/foo/baz/../bar/blub')
-        for test in tests:
+        if sys.platform == 'win32':
+            expected = ['C:\\foo\\bar\\baz\\']
+        else:
+            expected = ['/foo/bar/baz/']
+
+        for test in TEST_PATHS:
             rm = ResourceManager()
             rm.add_path('./baz/', test)
-            self.assertEqual(rm.path, ['/foo/bar/baz/'])
+            self.assertEqual(rm.path, expected)
 
-        for test in tests:
+        for test in TEST_PATHS:
             rm = ResourceManager()
             rm.add_path('baz/', test)
-            self.assertEqual(rm.path, ['/foo/bar/baz/'])
+            self.assertEqual(rm.path, expected)
 
     def test_path_order(self):
         rm = ResourceManager()
         rm.add_path('/middle/')
         rm.add_path('/first/', index=0)
         rm.add_path('/last/')
-        self.assertEqual(rm.path, ['/first/', '/middle/', '/last/'])
+
+        if sys.platform == 'win32':
+            self.assertEqual(rm.path, ['C:\\first\\', 'C:\\middle\\', 'C:\\last\\'])
+        else:
+            self.assertEqual(rm.path, ['/first/', '/middle/', '/last/'])
 
     def test_get(self):
         rm = ResourceManager()
